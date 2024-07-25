fbab1f4f50b6c761b3e0b3ad863c67d613315cc5
Igor <55831383+LisRecruit@users.noreply.github.com>
2024-07-25 22:50:28 +0300

    Update MyBot.java
    Try to fix buttons

--- src/main/java/BanksRequests/MyBot.java
+++ src/main/java/BanksRequests/MyBot.java
@@ -16,10 +16,6 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
-
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
