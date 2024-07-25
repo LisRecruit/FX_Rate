2584c36e70b76da6effae6c1441557a1888dee02
Kateryna <kateryna299@gmail.com>
2024-07-25 22:15:20 +0200

    buttons

--- src/main/java/BanksRequests/MyBot.java
+++ src/main/java/BanksRequests/MyBot.java
@@ -12,6 +12,8 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
