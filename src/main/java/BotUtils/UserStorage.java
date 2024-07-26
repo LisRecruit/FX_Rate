e4945d2a546d1723d255600d95d4c201395cc5a3
Igor <igorlis159@gmail.com>
2024-07-26 16:35:52 +0300

    Add switchNotification() to User

--- src/main/java/BotUtils/UserStorage.java
+++ src/main/java/BotUtils/UserStorage.java
@@ -3,7 +3,7 @@ package BotUtils;
 import java.util.HashMap;
 import java.util.Map;
 
-public class UserSettings {
+public class UserStorage {
     private static final Map<Long, User> users = new HashMap<>();
 
     public static User getUser(long chatId) {
